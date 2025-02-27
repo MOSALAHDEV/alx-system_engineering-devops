7bdfde3e8be71b2738713859c281f7834d21c273
MOSALAHDEV <mosalah525446@gmail.com>
2025-02-27 03:53:43 +0000

    add

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,8 @@
 # Fix Apache 500 error
+package { ['apache2', 'php', 'php-mysql']:
+  ensure  => installed,
+}
+
 file { '/var/www/html/index.php':
   ensure  => file,
   content => '<?php phpinfo(); ?>\n',
@@ -6,9 +10,16 @@ file { '/var/www/html/index.php':
   group   => 'www-data',
   mode    => '0644',
 }
+file { '/var/www/html/index.html':
+  ensure  => file,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0644',
+  recurse => true,
+}
 exec { 'apache2':
   ensure    => running,
   enable    => true,
   harestart => true,
-  subscribe => File['/var/www/html/index.php'],
+  subscribe => File['/var/www/html/index.php'], package['php-mysql'],
 }
