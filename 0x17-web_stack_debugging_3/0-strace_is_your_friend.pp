69914e110e39a0bc848f7fb4db9c1ca312ec736d
MOSALAHDEV <mosalah525446@gmail.com>
2025-02-27 14:07:03 +0000

    add

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,16 @@
-# fix  errors
+# fix errors
 
-exec{'fix-apache':
+file { '/var/www/html/web-settings.php':
+  ensure  => 'file',
+  content => 'initial content here',
+  mode    => '0644',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
+
+exec { 'fix-apache':
   command => 'sed -i s/phpp/php/g /var/www/html/web-settings.php',
   path    => ['/bin', '/usr/bin', '/usr/local/bin'],
+  onlyif  => 'test -f /var/www/html/web-settings.php',
 }
+
