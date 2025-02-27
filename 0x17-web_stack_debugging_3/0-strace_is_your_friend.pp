04ee0c009b7aa2005bc9ae6cd03a3a1468b20c4a
MOSALAHDEV <mosalah525446@gmail.com>
2025-02-27 14:01:22 +0000

    add

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,16 +1,6 @@
-# fix Apache 500 error by ensuring index.php has correct content
+# fix  errors
 
-file { '/var/www/html/index.php':
-  ensure  => file,
-  content => "<?php phpinfo(); ?>\n",
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
-}
-
-service { 'apache2':
-  ensure     => running,
-  enable     => true,
-  hasrestart => true,
-  subscribe  => [File['/var/www/html/index.php']],
+exec{'fix-apache':
+  command => 'sed -i s/phpp/php/g /var/www/html/web-settings.php',
+  path    => ['/bin', '/usr/bin', '/usr/local/bin'],
 }
