0327c9130d84462b8f2519539ee7882503bb66ba
MOSALAHDEV <mosalah525446@gmail.com>
2025-02-27 03:34:10 +0000

    add

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,13 +1,14 @@
-# Fix Apache 500 error
-file { '/var/www/html/index.php':
+le { '/var/www/html/index.php':
   ensure  => file,
-  content => '<?php phpinfo(); ?>\n',
+  content => "<?php phpinfo(); ?>\n",
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0644',
 }
-exec { 'restart_apache2':
-  command => '/bin/systemctl restart apache2',
-  path    => ['/bin', '/usr/bin'],
-  onlyif  => '/bin/systemctl is-active apache2',
+
+service { 'apache2':
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
+  subscribe  => File['/var/www/html/index.php'],
 }
