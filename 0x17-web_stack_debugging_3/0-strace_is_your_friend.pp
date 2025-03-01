7abd35198f85a6089141e7a6df8acef2c26fef6a
MOSALAHDEV <mosalah525446@gmail.com>
2025-03-01 04:09:00 +0000

    add

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,15 +1,6 @@
 # puppet script to correct typo in a file
 
-$file = '/var/www/html/wp-settings.php'
-
-exec { 'fix_wp_settings':
-  command => "sed -i 's/phpp/php/g' ${file}",
-  onlyif  => "grep -q 'phpp' ${file}",
-  path    => ['/bin', '/usr/bin'],
-}
-
-service { 'apache2':
-  ensure   => running,
-  enable   => true,
-  provider => 'init',
+exec { 'fix':
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  path    => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/bin'],
 }
