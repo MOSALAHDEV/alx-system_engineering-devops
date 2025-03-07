e5b21021e18151c317f69e7762dab766443af81e
MOSALAHDEV <mosalah525446@gmail.com>
2025-03-07 02:44:48 +0000

    adf

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,7 @@
-# This script increase nginx ability to handle traffic
-{ 'fix_nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx && sudo service nginx restart',
-  path    => '/usr/local/bin:/bin/',
+# This script increases Nginx's ability to handle traffic
+
+exec { 'fix_nginx':
+  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx && systemctl restart nginx',
+  path    => ['/usr/local/bin', '/bin'],
 }
+
